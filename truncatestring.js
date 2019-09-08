e1874be13806d1cb1699f418b6213956b5a0a5fe
avacallery <avacallery@gmail.com>
2019-09-08 19:30:30 -0400

    commented out var for truncate string

--- truncatestring.js
+++ truncatestring.js
@@ -1,6 +1,6 @@
 function truncateString(str,num) { 
     var truncatedString = "";
-    if (var i = 0; )
+    // if (var i = 0; )
     str.slice(str.length - num.length) === truncatedString + "...";
     return str;
 }
