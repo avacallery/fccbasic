554e551d033c368cd43bc1746c3ea4cc15a3941b
avacallery <avacallery@gmail.com>
2019-09-09 10:08:39 -0400

    truncate string final

--- truncatestring.js
+++ truncatestring.js
@@ -1,9 +1,22 @@
 function truncateString(str,num) { 
     var truncatedString = "";
-    // if (var i = 0; )
+    // if ( )
     str.slice(str.length - num.length) === truncatedString + "...";
     return str;
 }
 
 
 console.log(truncateString("A-tisket a-tasket a green a yellow basket", 8)); 
+
+
+//function truncateString(str, num) {
+//   // Clear out that junk in your trunk
+//   if (str.length > num && num > 3) {
+//     return str.slice(0, (num - 3)) + '...';
+//   } else if (str.length > num && num <= 3) {
+//     return str.slice(0, num) + '...';
+//   } else {
+//     return str;
+//   }
+
+// }
